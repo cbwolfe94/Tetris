82f091a38f451b042aa159b339c4ebc07088122c
cbwolfe94 <cbwolfe94@hotmail.com>
2021-01-06 13:34:30 -0800

    Tetromino struct working, logic faulty

--- tetromino.c
+++ tetromino.c
@@ -1,5 +1,8 @@
 
 #include "tetromino.h"
+const int L_PIECE_COORDINATES[4][2] = {{4, 35}, {5, 35}, {6, 35}, {7,35}};
+const int O_PIECE_COORDINATES[4][2] = {{9, 35}, {10, 35}, {9, 37}, {10, 37}}; 
+
 
 
 struct tetromino {
@@ -8,7 +11,7 @@ struct tetromino {
     // int startPositionY;
     
     //Location of cells of each tetromino block
-    int blockPosition;
+    //int blockPosition[4][2];
 
     //int fallSpeed;
 
@@ -17,6 +20,7 @@ struct tetromino {
 tetromino* createTetromino() {
     
     tetromino* block = (tetromino*)malloc(sizeof(tetromino));
+    //block->blockPosition = DEFAULT_STARTING_COORDINATE;
     // block->typeOfBlock = (char*)malloc(strlen(typeOfBlock));
     // strcpy(block->typeOfBlock, typeOfBlock);
     return block;
@@ -33,8 +37,8 @@ void moveTetromino(tetromino* block, char* direction) {
 }
 
 void displayTetromino(tetromino* block) {
-    for (int row = 0; row < 3; row++) {
-        mvaddstr(block->blockPosition[row][0], block->blockPosition[row][1], "[]");
+    for (int row = 0; row < 4; row++) {
+        mvaddstr(O_PIECE_COORDINATES[row][0], O_PIECE_COORDINATES[row][1], "[]");
     }
 }
 
